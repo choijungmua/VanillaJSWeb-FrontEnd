ae1406b5bd6098614357a89bbfd5bb500df6d8e0
Jungmu <wjdan022@naver.com>
2024-02-25 02:25:31 +0900

    css ui정리 및 text추가.

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,6 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
-
 let toDos = [];
 const TODOS_KEY = "todos";
 //저장하는 함수
@@ -20,6 +19,9 @@ function deleteToDo(event) {
 
 // li와 span을 만들어내 출력하는 함수.
 function paintToDo(newTodo) {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.class = "checkbox";
   const li = document.createElement("li");
   li.id = newTodo.id;
   const span = document.createElement("span");
@@ -27,13 +29,13 @@ function paintToDo(newTodo) {
   span.innerText = newTodo.text;
   button.innerText = "X";
   button.addEventListener("click", deleteToDo);
+  li.appendChild(checkbox);
   li.appendChild(span);
   li.appendChild(button);
 
   toDoList.appendChild(li);
 }
 
-// input에 값이 들어왔을때 paintToDo를 출력시키는 함수.
 function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
